0db709ce4ab8632108e823c07095b3a4ac130359
WebMats <lorlando127@gmail.com>
2019-03-01 11:58:45 -0500

    completed MVP

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
 
-const jwtKey =
-  process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+const jwtKey = process.env.JWT_SECRET || 'add a .env file to root of project with the JWT_SECRET variable';
 
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  jwtKey
 };
 
 // implementation details
